d5e4b48dd5a1464854120140df46f3110ed86037
Geraint White <geraint@android.net>
2014-04-08 00:09:03 +0100

    Added newline

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ http.createServer(function(req, response) {
         body += chunk.toString();
       });
 
+
       req.on('end', function() {
         last_payload = JSON.parse(body);
         console.log(new Date(), req.method, req.url);
