972fcab9c25c2c3857bde94dd7cfce287564af42
Geraint White <geraint@android.net>
2014-04-08 22:40:00 +0100

    Possible fix

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-var socket = io.connect();
+var socket = io.connect(
+    'http://node.dvbris.com/git/'
+    , {resource: 'git/socket.io'}
+);
 socket.on('update_out', function (data) {
     console.log(data);
 });
