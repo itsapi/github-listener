5010bd9ee82be6b2682d08fe33e070edba65f775
Oliver Faircliff <faircliff.oliver@gmail.com>
2014-04-08 22:14:50 +0100

    Possible fix

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://node.dvbris.com', { resource: 'git/socket.io' });
+var socket = io.connect('http://node.dvbris.com', { resource: 'git' });
 socket.on('update_out', function (data) {
     console.log(data);
 });
