18ec9041644a3fd1bfa6e3f54061315f51ccbea5
Geraint White <geraint@android.net>
2014-04-27 17:29:54 +0100

    Added check to only run Ajax when window in focus

--- main.js
+++ main.js
@@ -35,11 +35,16 @@ var script_out = document.getElementById('right').getElementsByTagName('pre')[0]
 var timestamp = document.getElementById('time');
 
 setInterval(function () {
-  makeRequest('http://node.dvbris.com/git?refresh', function (data) {
-    data = JSON.parse(data);
+  if (!window.blurred) {
+    makeRequest('http://node.dvbris.com/git?refresh', function (data) {
+      data = JSON.parse(data);
 
-    last_payload.innerText = JSON.stringify(data.last_payload, null, '  ');
-    script_out.innerHTML = data.script_out;
-    timestamp.innerText = data.timestamp;
-  });
+      last_payload.innerText = JSON.stringify(data.last_payload, null, '  ');
+      script_out.innerHTML = data.script_out;
+      timestamp.innerText = data.timestamp;
+    });
+  }
 }, 2000);
+
+window.onblur = function() { window.blurred = true; };
+window.onfocus = function() { window.blurred = false; };
