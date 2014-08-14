bb0af6c58c0481e6795941711092c967ced4e743
Geraint White <geraint@android.net>
2014-08-14 23:06:08 +0100

    Added support for different domains

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ var script_out = document.getElementById('right').getElementsByTagName('pre')[0]
 var header = document.getElementsByTagName('header')[0];
 
 setInterval(function () {
-  makeRequest('http://git.dvbris.com?refresh', function (data) {
+  makeRequest(window.location.href+'?refresh', function (data) {
     data = JSON.parse(data);
 
     last_payload.innerHTML = JSON.stringify(data.last_payload, null, '  ');
