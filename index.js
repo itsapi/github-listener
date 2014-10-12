f85a11abb21aa6a546bc84cd3efa640abf3ab349
Oliver Faircliff <mail@oliverfaircliff.com>
2014-10-12 17:11:59 +0100

    Cleaned up the functions

--- index.js
+++ index.js
@@ -6,28 +6,6 @@ var app = require('http').createServer(handler),
     jade = require('jade'),
     fs = require('fs');
 
-var last_payload = {};
-var script_out = '';
-var status = 'Ready';
-var header = '';
-var timestamp = new Date();
-var running = false;
-
-var SECRET;
-fs.readFile(__dirname + '/secret.txt', function(err, data) {
-  if (err) throw err;
-  data = data.toString();
-  while (data.slice(-1) == '\n') {
-    data = data.slice(0, -1);
-  }
-  SECRET = data;
-});
-
-var template;
-fs.readFile(__dirname + '/index.jade', function(err, data) {
-  if (err) throw err;
-  template = jade.compile(data.toString(), {pretty: true});
-});
 
 function sendFile(res, path, type) {
   fs.readFile(path, function (err, data) {
@@ -38,6 +16,7 @@ function sendFile(res, path, type) {
     res.end(text);
   });
 }
+
 
 function toHtml(string) {
   // Converts URLs to HTML links
@@ -55,6 +34,7 @@ function genHeader() {
   }
 }
 
+
 function assembleData(format) {
   return {
     last_payload: format ? JSON.stringify(last_payload, null, '  ') : last_payload,
@@ -64,40 +44,46 @@ function assembleData(format) {
   };
 }
 
+
+function serve(url_parts, res) {
+  switch (url_parts.pathname) {
+    case '/':
+      if (url_parts.query.refresh === undefined) { // Send the HTML
+        var html = template(assembleData(true));
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+
+      } else { // Send the data
+        console.log('Data requested by GET');
+        genHeader();
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(assembleData()));
+      }
+      break;
+
+    case '/main.js':
+      console.log('Sending JS');
+      sendFile(res, __dirname + '/static/main.js', 'application/javascript');
+      break;
+
+    case '/main.css':
+      console.log('Sending CSS');
+      sendFile(res, __dirname + '/static/main.css', 'text/css');
+      break;
+
+    default:
+      console.log('404: ' + url_parts.pathname);
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('404 - File not found: ' + url_parts.pathname);
+  }
+}
+
+
 function handler(req, res) {
   var url_parts = url.parse(req.url, true);
 
   if (req.method == 'GET') {
-    switch (url_parts.pathname) {
-      case '/':
-        if (url_parts.query.refresh === undefined) { // Send the HTML
-          var html = template(assembleData(true));
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.end(html);
-
-        } else { // Send the data
-          console.log('Data requested by GET');
-          genHeader();
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          res.end(JSON.stringify(assembleData()));
-        }
-        break;
-
-      case '/main.js':
-        console.log('Sending JS');
-        sendFile(res, __dirname + '/main.js', 'application/javascript');
-        break;
-
-      case '/main.css':
-        console.log('Sending CSS');
-        sendFile(res, __dirname + '/main.css', 'text/css');
-        break;
-
-      default:
-        console.log('404: ' + url_parts.pathname);
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('404 - File not found: ' + url_parts.pathname);
-    }
+    serve(url_parts, res);
   } else {
 
     var secret = url_parts.pathname.slice(1);
@@ -161,6 +147,35 @@ function handler(req, res) {
     }
   }
 }
+
+
+var last_payload = {};
+var script_out = '';
+var status = 'Ready';
+var header = '';
+var timestamp = new Date();
+var running = false;
+
+
+// Load the secret from the file
+var SECRET;
+fs.readFile(__dirname + '/secret.txt', function(err, data) {
+  if (err) throw err;
+  data = data.toString();
+  while (data.slice(-1) == '\n') {
+    data = data.slice(0, -1);
+  }
+  SECRET = data;
+});
+
+
+// Load the Jade template
+var template;
+fs.readFile(__dirname + '/index.jade', function(err, data) {
+  if (err) throw err;
+  template = jade.compile(data.toString(), {pretty: true});
+});
+
 
 io.on('connection', function(socket) {
   events.on('refresh', function() {
