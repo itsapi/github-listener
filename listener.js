ca875962ef0c1146de8f6ad42c9baed79668912d
Oliver Faircliff <mail@oliverfaircliff.com>
2014-11-20 15:07:53 +0000

    Moved git stuff into it`s own module

---
+++ listener.js
@@ -0,0 +1,135 @@
+var url = require('url'),
+    exec = require('child_process').exec,
+    events = new (require('events').EventEmitter)(),
+    crypto = require('crypto'),
+    bl = require('bl');
+
+require('string-format');
+
+
+// Verify payload signature
+function verify_payload(signature, secret, payload) {
+  var hash = 'sha1=' + crypto.createHmac('sha1', secret).update(payload).digest('hex');
+  return signature == hash;
+}
+
+
+var Listener = function (config) {
+  this.config = config;
+  this.timestamp = new Date();
+  this.running = false;
+  this.last_payload = {};
+  this.script_out = '';
+  this.status = 'Ready';
+
+  this.hook = function (url_parts, req, res) {
+    // Get payload
+    var self = this;
+    req.pipe(bl(function (err, data) {
+      if (err) {
+        self.respond(res, 400, 'Error whilst receiving payload');
+        self.status = 'Error';
+        return false;
+      }
+
+      self.run_when_ready(function () {
+        self.running = true;
+
+        try {
+          self.last_payload = JSON.parse(data);
+        } catch (e) {
+          self.respond(res, 400, 'Error: Invalid payload');
+          self.status = 'Error';
+          self.running = false;
+          return false;
+        }
+
+        console.log(new Date(), req.method, req.url);
+        console.log(JSON.stringify(self.last_payload, null, '\t') + '\n');
+
+        // Verify payload signature
+        signature = req.headers['x-hub-signature'];
+        if (!(signature && verify_payload(signature, self.config.secret, data))) {
+          self.respond(res, 401, 'Error: Cannot verify payload signature');
+          self.status = 'Error';
+          self.running = false;
+          return false;
+        }
+
+        // Check we have the information we need
+        if (!(self.last_payload.repository && self.last_payload.repository.full_name)) {
+          self.respond(res, 400, 'Error: Invalid data');
+          self.status = 'Error';
+          self.running = false;
+          return false;
+        }
+
+        // Run script
+        self.respond(res, 200, 'Waiting for script to finish');
+        self.status = 'Waiting';
+
+        branch = url.parse(req.url).pathname.replace(/^\/|\/$/g, '') || 'master';
+
+        var out = ''
+        self.getter(self.last_payload.repository.full_name, branch, function (getter_out) {
+          out += getter_out;
+          self.post_receive(function (post_receive_out) {
+            out += post_receive_out;
+            console.log('\n' + out);
+            console.log('Finished processing files\n');
+
+            self.script_out = out;
+            self.status = 'Done';
+            self.running = false;
+            self.timestamp = new Date();
+            events.emit('refresh');
+          });
+        });
+      });
+    }));
+  }
+
+  this.getter = function (repo, branch, cb) {
+    var command = this.config.getter.format({
+      repo_dir: this.config.repo_dir,
+      output: this.config.processing,
+      repo: repo,
+      branch: branch
+    });
+    console.log(command)
+    exec(command, function(error, stdout, stderr) {
+      cb(stdout + stderr);
+    });
+  }
+
+  this.post_receive = function(cb) {
+    var command = this.config.post_receive.format({dir: this.config.processing});
+    console.log(command)
+    exec(command, function(error, stdout, stderr) {
+      cb(stdout + stderr);
+    });
+  }
+
+  this.run_when_ready = function(func) {
+    // Avoids running multiple requests at once.
+    if (this.running) console.log('Script already running');
+    function wait() {
+      if (this.running) setTimeout(wait, 100);
+      else func();
+    }
+    wait();
+  }
+
+  this.respond = function(res, http_code, message) {
+    console.log(message);
+
+    this.script_out = message;
+    events.emit('refresh');
+
+    res.writeHead(http_code, {'Content-Type': 'text/plain'});
+    res.end(message);
+  }
+};
+
+
+module.exports = Listener;
---
+++ server.js
@@ -0,0 +1,124 @@
+var http = require('http'),
+    socketio = require('socket.io'),
+    url = require('url'),
+    events = new (require('events').EventEmitter)(),
+    jade = require('jade'),
+    fs = require('fs'),
+    config = require('./config.json'),
+    Listener = require('./listener');
+
+
+function gen_header() {
+  header = listener.timestamp.toString();
+  if (listener.last_payload.repository && listener.last_payload.head_commit) {
+    header += ' | Commit: ' + listener.last_payload.head_commit.message +
+              ' | URL: ' + listener.last_payload.repository.url;
+  }
+}
+
+
+function send_file(res, path, type) {
+  fs.readFile(path, function (err, data) {
+    if (err) throw err;
+
+    res.writeHead(200, {'Content-Type': type});
+    res.end(data);
+  });
+}
+
+
+function to_html(string) {
+  // Converts URLs to HTML links
+  return string.replace(
+    /((https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?)/g,
+    '<a href=\"$1\">$1</a>'
+  );
+}
+
+
+function assemble_data(format) {
+  return {
+    last_payload: format ? JSON.stringify(listener.last_payload, null, '  ') : listener.last_payload,
+    script_out: to_html(listener.script_out),
+    header: to_html(header),
+    status: listener.status
+  };
+}
+
+
+function serve(url_parts, res) {
+  if (url_parts.pathname == '/') {
+    if (url_parts.query.refresh === undefined) { // Send the HTML
+      var html = template(assemble_data(true));
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(html);
+
+    } else { // Send the data
+      console.log('Data requested by GET');
+      gen_header();
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(assemble_data()));
+    }
+
+  } else if (url_parts.pathname == '/main.js') {
+    console.log('Sending JS');
+    send_file(res, __dirname + '/static/main.js', 'application/javascript');
+
+  } else if (url_parts.pathname == '/main.css') {
+    console.log('Sending CSS');
+    send_file(res, __dirname + '/static/main.css', 'text/css');
+
+  } else if (url_parts.pathname.match(/\/icons\/\w*/)) {
+    var name = url_parts.pathname.split('/').slice(-1)[0].toLowerCase();
+    console.log('Sending icon: ' + name);
+    send_file(res, __dirname + '/static/icons/' + name + '.png', 'image/png');
+
+  } else {
+    console.log('404: ' + url_parts.pathname);
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('404 - File not found: ' + url_parts.pathname);
+  }
+}
+
+
+var header = '';
+
+// Make listener
+var listener = new Listener(config);
+
+
+// Load the Jade template
+var template;
+fs.readFile(__dirname + '/index.jade', function(err, data) {
+  if (err) throw err;
+  template = jade.compile(data.toString(), {pretty: true});
+});
+
+
+// Setup server
+var app = http.createServer(function (req, res) {
+  var url_parts = url.parse(req.url, true);
+
+  if (req.method == 'GET') {
+    serve(url_parts, res);
+  } else {
+    listener.hook(url_parts, req, res);
+  }
+});
+
+
+// Set up the socket to send new data to the client.
+var io = socketio(app);
+io.on('connection', function(socket) {
+  events.on('refresh', function() {
+    console.log('Data sent by socket');
+    gen_header();
+    socket.emit('refresh', JSON.stringify(assemble_data()));
+  });
+});
+
+
+// Start the server
+var port = 6003;
+app.listen(port);
+console.log('Server running on port', port);
