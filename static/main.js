08d0b6ba58ff794f80832d38982d841332f4cf72
Oliver Faircliff <mail@oliverfaircliff.com>
2015-05-15 22:32:50 +0100

    Semicolons

--- static/main.js
+++ static/main.js
@@ -2,35 +2,35 @@ document.head = document.head || document.getElementsByTagName('head')[0];
 
 
 function makeRequest(url, cb) {
-  var httpRequest
+  var httpRequest;
   if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-    httpRequest = new XMLHttpRequest()
+    httpRequest = new XMLHttpRequest();
   } else if (window.ActiveXObject) { // IE
     try {
-      httpRequest = new ActiveXObject("Msxml2.XMLHTTP")
+      httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
     }
     catch (e) {
       try {
-        httpRequest = new ActiveXObject("Microsoft.XMLHTTP")
+        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
       }
       catch (e) {}
     }
   }
   if (!httpRequest) {
-    console.log('Giving up :( Cannot create an XMLHTTP instance')
-    return false
+    console.log('Giving up :( Cannot create an XMLHTTP instance');
+    return false;
   }
   httpRequest.onreadystatechange = function () {
     if (httpRequest.readyState === 4) {
       if (httpRequest.status === 200) {
-        cb(httpRequest.responseText)
+        cb(httpRequest.responseText);
       } else {
-        console.log('There was a problem with the request.')
+        console.log('There was a problem with the request.');
       }
     }
-  }
-  httpRequest.open('GET', url)
-  httpRequest.send()
+  };
+  httpRequest.open('GET', url);
+  httpRequest.send();
 }
 
 
