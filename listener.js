fd5345e751aeca448a57bf132af52ff2df22fa36
Geraint White <geraintwhite@gmail.com>
2015-06-29 20:15:23 +0100

    Make listener.js a prototype based class

--- listener.js
+++ listener.js
@@ -8,134 +8,152 @@ var url = require('url')
 format.extend(String.prototype);
 
 
-var Listener = function (config, logs) {
-  this.config = config;
-  this.timestamp = new Date();
-  this.running = false;
-  this.last_payload = {};
-  this.data = {};
-  this.script_out = '';
-  this.status = 'Ready';
+function Listener (config, logging) {
+  var self = this;
 
-  this.error = function (res, code, message) {
-    this.status = 'Error';
-    this.running = false;
-    this.respond(res, code, message);
-  };
+  self.config = config;
+  self.logging = logging;
+  self.timestamp = new Date();
+  self.running = false;
+  self.last_payload = {};
+  self.data = {};
+  self.script_out = '';
+  self.status = 'Ready';
+}
 
-  this.hook = function (req, res) {
-    // Get payload
-    var self = this;
-    req.pipe(bl(function (err, data) {
-      if (err) return self.error(res, 400, 'Error whilst receiving payload');
+Listener.prototype.error = function (res, code, message) {
+  var self = this;
 
-      self.run_when_ready(function () {
-        self.running = true;
+  self.status = 'Error';
+  self.running = false;
+  self.respond(res, code, message);
+};
 
-        if (req.headers['travis-repo-slug'])
-          self.parser = new parser.Travis(data, req.headers, self.config);
-        else self.parser = new parser.GitHub(data, req.headers, self.config);
+Listener.prototype.hook = function (req, res) {
+  var self = this;
 
-        self.last_payload = self.parser.parse_body();
-        if (!self.last_payload) return self.error(res, 400, 'Error: Invalid payload');
+  // Get payload
+  req.pipe(bl(function (err, data) {
+    if (err) return self.error(res, 400, 'Error whilst receiving payload');
 
-        self.log(new Date(), req.method, req.url);
-        self.log(JSON.stringify(self.last_payload, null, '\t') + '\n');
+    self.run_when_ready(function () {
+      self.running = true;
 
-        // Verify payload signature
-        if (!self.parser.verify_signature())
-          return self.error(res, 403, 'Error: Cannot verify payload signature');
+      if (req.headers['travis-repo-slug'])
+        self.parser = new parser.Travis(data, req.headers, self.config);
+      else self.parser = new parser.GitHub(data, req.headers, self.config);
 
-        // Check we have the information we need
-        self.data = self.parser.extract();
-        if (!self.data) return self.error(res, 400, 'Error: Invalid data');
+      self.last_payload = self.parser.parse_body();
+      if (!self.last_payload) return self.error(res, 400, 'Error: Invalid payload');
 
-        // Check branch in payload matches branch in URL
-        var repo = self.data.slug;
-        var branch = url.parse(req.url).pathname.replace(/^\/|\/$/g, '') || 'master';
-        if (self.data.branch != branch) {
-          return self.error(res, 202, 'Branches do not match', true);
-        }
+      self.log(new Date(), req.method, req.url);
+      self.log(JSON.stringify(self.last_payload, null, '\t') + '\n');
 
-        self.build = (function (repo, branch) {
-          return function (res) {
-            // Run script
-            self.status = 'Waiting';
-            self.respond(res, 200, 'Waiting for script to finish');
+      // Verify payload signature
+      if (!self.parser.verify_signature())
+        return self.error(res, 403, 'Error: Cannot verify payload signature');
 
-            var out = '';
-            self.getter(repo, branch, function (getter_out) {
-              out += getter_out;
-              self.post_receive(repo, function (post_receive_out) {
-                out += post_receive_out;
-                self.log('\n' + out);
-                self.log('Finished processing files\n');
+      // Check we have the information we need
+      self.data = self.parser.extract();
+      if (!self.data) return self.error(res, 400, 'Error: Invalid data');
 
-                self.script_out = out;
-                self.status = 'Done';
-                self.running = false;
-                self.timestamp = new Date();
-                process.emit('refresh');
-              });
+      // Check branch in payload matches branch in URL
+      var repo = self.data.slug;
+      var branch = url.parse(req.url).pathname.replace(/^\/|\/$/g, '') || 'master';
+      if (self.data.branch != branch) {
+        return self.error(res, 202, 'Branches do not match', true);
+      }
+
+      self.build = (function (repo, branch) {
+        return function (res) {
+          // Run script
+          self.status = 'Waiting';
+          self.respond(res, 200, 'Waiting for script to finish');
+
+          var out = '';
+          self.getter(repo, branch, function (getter_out) {
+            out += getter_out;
+            self.post_receive(repo, function (post_receive_out) {
+              out += post_receive_out;
+              self.log('\n' + out);
+              self.log('Finished processing files\n');
+
+              self.script_out = out;
+              self.status = 'Done';
+              self.running = false;
+              self.timestamp = new Date();
+              process.emit('refresh');
             });
-          };
-        })(repo, branch);
-        self.build(res);
-      });
-    }));
-  };
-
-  this.build = function (res) {
-    this.respond(res, 200, 'Nothing to build');
-  };
-
-  this.getter = function (repo, branch, cb) {
-    var command = this.config.getter.format(
-    { repo_dir: this.config.repo_dir
-    , output: this.config.processing
-    , repo: repo
-    , branch: branch
+          });
+        };
+      })(repo, branch);
+      self.build(res);
     });
-    this.log(command);
-    exec(command, function(error, stdout, stderr) {
-      cb(stdout + stderr);
-    });
-  };
+  }));
+};
 
-  this.post_receive = function (repo, cb) {
-    var command = this.config.post_receive
-                  .format({dir: this.config.processing, name: repo});
-    this.log(command);
-    exec(command, function(error, stdout, stderr) {
-      cb(stdout + stderr);
-    });
-  };
+Listener.prototype.build = function (res) {
+  var self = this;
 
-  this.run_when_ready = function (func) {
-    // Avoids running multiple requests at once.
-    if (this.running) this.log('Script already running');
-    function wait() {
-      if (this.running) setTimeout(wait, 100);
-      else func();
-    }
-    wait();
-  };
+  self.respond(res, 200, 'Nothing to build');
+};
 
-  this.respond = function (res, http_code, message, not_refresh) {
-    this.log(message);
+Listener.prototype.getter = function (repo, branch, cb) {
+  var self = this;
 
-    if (not_refresh === undefined) {
-      this.script_out = message;
-      process.emit('refresh');
-    }
+  var command = self.config.getter.format(
+  { repo_dir: self.config.repo_dir
+  , output: self.config.processing
+  , repo: repo
+  , branch: branch
+  });
+  self.log(command);
+  exec(command, function(error, stdout, stderr) {
+    cb(stdout + stderr);
+  });
+};
 
-    res.writeHead(http_code, {'Content-Type': 'text/plain'});
-    res.end(message);
-  };
+Listener.prototype.post_receive = function (repo, cb) {
+  var self = this;
 
-  this.log = function (msg) {
-    if (logs) console.log(msg);
-  };
+  var command = self.config.post_receive
+                .format({dir: self.config.processing, name: repo});
+  self.log(command);
+  exec(command, function(error, stdout, stderr) {
+    cb(stdout + stderr);
+  });
+};
+
+Listener.prototype.run_when_ready = function (func) {
+  var self = this;
+
+  // Avoids running multiple requests at once.
+  if (self.running) self.log('Script already running');
+  function wait() {
+    if (self.running) setTimeout(wait, 100);
+    else func();
+  }
+  wait();
+};
+
+Listener.prototype.respond = function (res, http_code, message, not_refresh) {
+  var self = this;
+
+  self.log(message);
+
+  if (not_refresh === undefined) {
+    self.script_out = message;
+    process.emit('refresh');
+  }
+
+  res.writeHead(http_code, {'Content-Type': 'text/plain'});
+  res.end(message);
+};
+
+Listener.prototype.log = function () {
+  var self = this;
+
+  if (self.logging) console.log.apply(null, arguments);
 };
 
 
