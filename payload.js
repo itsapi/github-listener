ec6856eccb228c653d3f40640d65dbf16835ba67
Geraint White <geraintwhite@gmail.com>
2015-07-24 13:41:36 +0100

    Update example payload

--- payload.js
+++ payload.js
@@ -14,13 +14,13 @@ var options = {
 
 
 if (process.argv[2] === 'travis') {
-  var slug = 'repo';
+  var slug = 'user/repo';
 
   payload = qs.stringify({
     payload: JSON.stringify({
       repository: { url: 'http://example.com' },
       branch: 'master',
-      message: 'example commit'
+      message: 'did some stuff'
     })
   });
 
@@ -32,7 +32,7 @@ if (process.argv[2] === 'travis') {
 
 } else {
   payload = JSON.stringify({
-    repository: { full_name: 'repo', url: 'http://example.com' },
+    repository: { full_name: 'user/repo', url: 'http://example.com' },
     ref: 'refs/heads/master',
     head_commit: { message: 'example commit' }
   });
