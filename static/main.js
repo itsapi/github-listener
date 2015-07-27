a1cca55834aea8bc28405148b09a1f2366fb3a7f
Geraint White <geraintwhite@gmail.com>
2015-07-27 22:15:24 +0100

    Remove makeRequest

--- static/main.js
+++ static/main.js
@@ -166,37 +166,3 @@ function setStatusTitle(status) {
   if (oldLink) { document.head.removeChild(oldLink); }
   document.head.appendChild(link);
 }
-
-/*
-function makeRequest(url, cb) {
-  var httpRequest;
-  if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-    httpRequest = new XMLHttpRequest();
-  } else if (window.ActiveXObject) { // IE
-    try {
-      httpRequest = new ActiveXObject('Msxml2.XMLHTTP');
-    }
-    catch (e) {
-      try {
-        httpRequest = new ActiveXObject('Microsoft.XMLHTTP');
-      }
-      catch (e) {}
-    }
-  }
-  if (!httpRequest) {
-    console.log('Giving up :( Cannot create an XMLHTTP instance');
-    return false;
-  }
-  httpRequest.onreadystatechange = function () {
-    if (httpRequest.readyState === 4) {
-      if (httpRequest.status === 200) {
-        cb(httpRequest.responseText);
-      } else {
-        console.log('There was a problem with the request.');
-      }
-    }
-  };
-  httpRequest.open('GET', url);
-  httpRequest.send();
-}
-*/
